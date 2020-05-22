b3e52ac11d384aebd60a2efe1da0a30cd226dd43
Ruth Amaya <ruth.amaya@jbhunt.com>
2020-05-22 11:24:38 -0500

    profile pic path

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,6 @@ class About extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -20,7 +19,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Ruth Amaya Profile Pic" />
+            <img className="profile-pic"  src="public/images/profilepic.JPG" alt="Ruth Amaya Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
