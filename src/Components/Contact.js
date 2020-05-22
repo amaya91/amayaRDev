17a7a97dd99761ba39ae51c2ed21594f49e866cb
Ruth Amaya <ruth.amaya@jbhunt.com>
2020-05-22 11:12:32 -0500

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+
+                  <p className="lead">{message}</p>
+
+
+         </div>
+
+         <div className="row">
+
+            <div id="contact-widget"></div>
+
+
+					   <h4>Address and Phone</h4>
+					   <p className="address">
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span><br />
+                     <a href="mailto: ruthamaya91@gmail.com">{email}</a>
+
+					   </p>
+
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
