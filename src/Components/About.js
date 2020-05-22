755cc9d68b7207b2fc6dadb8515c9343cfb6b7a8
Ruth Amaya <ruth.amaya@jbhunt.com>
2020-05-22 11:54:16 -0500

    cleaned up about

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,7 +19,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src="public/images/profilepic.JPG" alt="Ruth Amaya Profile Pic" />
+            <img className="profile-pic"  src="../../public/images/profilepic.JPG" alt="Ruth Amaya Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
