285b0afa439c14ca68868274c7f8922ac6ea2f7b
Ruth Amaya <ruth.amaya@jbhunt.com>
2020-05-22 11:15:42 -0500

    Updates

---
+++ precache-manifest.18a282d42d06d92757ce0750850666f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f931a442e5a281e84bc392dca71e6a",
+    "url": "/amayaRDev/index.html"
+  },
+  {
+    "revision": "9adb4ef425612c0d5b3b",
+    "url": "/amayaRDev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14e6cb77661250122b53",
+    "url": "/amayaRDev/static/js/2.f8e6fee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amayaRDev/static/js/2.f8e6fee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adb4ef425612c0d5b3b",
+    "url": "/amayaRDev/static/js/main.31a7aeb6.chunk.js"
+  },
+  {
+    "revision": "90fa18743a37df291c7c",
+    "url": "/amayaRDev/static/js/runtime-main.e07e363e.js"
+  }
+]);
